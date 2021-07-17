e8959915a0cc9c6f49fc153d4d037996bf107738
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-07-17 17:49:14 +0600

    State of a Component

--- src/App.js
+++ src/App.js
@@ -5,41 +5,28 @@ import './App.css';
 import Book from './Components/Book';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello Sajib</h1>
-//       <Person/>
-//     </div>
-//   );
-// }
-// function App(){
-// return React.createElement('div', { className: "App" }, React.createElement('h1', null, "Hello World"),<Person/> );
-// }
-
-// function Person(){
-//   return(
-//     <div>
-//       <h1> I am Person Component</h1>
-//     </div>
-
-//   );
-// }
-// function Person(){
-//   return(React.createElement('div',null,React.createElement('h1',null,"I am Person Component")));
-// }
-
-
-
 class App extends Component{
+  // constructor(){
+  //   super();
+  //   this.state={}
+  // }
+  state={
+    books:[
+      {bookName:"1984",written:"Gerorge"},
+      {bookName:"x",written:"xy"},
+      {bookName:"y",written:"yz"},
+      {bookName:"z",written:"zx"}
+    ],
+    otherProp:"I am some other Prop"
+  }
   render(){
     return(
       <div className="App">
          <h1>Book List</h1>
-         <Book bookName="1984" written="George Orwell">Book Info</Book>
-         <Book bookName="1971" written="x">Book Info</Book>
-         <Book bookName="1947" written="y">Book Info</Book>
-         <Book bookName="1954" written="z">Book Info</Book>
+         <Book bookName={this.state.books[0].bookName} written={this.state.books[0].written}/>
+         <Book bookName={this.state.books[1].bookName} written={this.state.books[1].written}/>
+         <Book bookName={this.state.books[2].bookName} written={this.state.books[2].written}/>
+         <Book bookName={this.state.books[3].bookName} written={this.state.books[3].written}/> 
       </div>
     );
   }
