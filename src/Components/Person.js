a1fc76be423318c2ea4cef457d54a81fcbdf77fa
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-07-17 13:35:49 +0600

    Dynamic Class Component

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{Component} from 'react';
 // function Person(){
 //   return(
 //     <div>
@@ -8,12 +8,28 @@ import React from 'react';
 //   );
 // }
 
-let Person=(props)=>{
-return(
-     <div>
-        <h1> Name:{props.name} and Age:{props.age}</h1>
-        <h2> Extra Info:{props.children}</h2>
-     </div>
-      );
+// let Person=(props)=>{
+// return(
+//      <div>
+//         <h1> Name:{props.name} and Age:{props.age}</h1>
+//         <h2> Extra Info:{props.children}</h2>
+//      </div>
+//       );
+// }
+
+class Person extends Component {
+   constructor(props){
+     super(props);
+
+   }
+   render(){
+      console.log(this.props);
+      return(
+        <div>
+             <h1> Name:{this.props.name} and Age:{this.props.age}</h1>
+           <h2> Extra Info:{this.props.children}</h2>
+        </div>
+            );
+   }
 }
 export default Person;
