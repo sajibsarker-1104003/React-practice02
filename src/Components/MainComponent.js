6a9445b70647bbb7991a02739b80b900e19eccbf
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-07-26 17:39:56 +0600

    Better Project Structure:Clean Root Component

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,78 @@
+import React ,{Component} from 'react';
+import Book from './Book';
+class MainComponent extends Component{
+// constructor(){
+  //   super();
+  //   this.state={}
+  // }
+  state={
+    books:[
+      {id:1,bookName:"1984",written:"Gerorge"},
+      {id:2,bookName:"x",written:"xy"},
+      {id:3,bookName:"y",written:"yz"},
+      {id:4,bookName:"z",written:"zx"}
+    ],
+    showBooks:true
+  }
+ 
+ 
+  changeWithInputState=(event,index)=>{
+    const book={...this.state.books[index]
+    }
+    book.bookName=event.target.value;
+    const books=[...this.state.books];
+    books[index]=book;
+    this.setState({books:books});
+  }
+
+  deleteBookState=index=>{
+    //const books=this.state.books;
+    //const books=this.state.books.slice();
+    //const books=this.state.books.map(item=>item);
+    const books=[...this.state.books];
+    books.splice(index,1);
+    this.setState({
+      books:books
+    });
+      };
+      toggleBooks=()=>{
+      this.setState({ showBooks:!this.state.showBooks})
+      }
+
+  render(){
+    const style={
+      border:"1 px solid red",
+      borderRadius:"5px",
+      backgroundColor:"black",
+      color:"white"
+    };
+    
+    let books=null;
+    if(this.state.showBooks){
+       books=this.state.books.map((book,index)=>{
+        return(
+           <Book
+           bookName={book.bookName}
+           written={book.written}
+           delete={this.deleteBookState.bind(this,index)}
+           //delete={()=>this.deleteBookState(index)}
+           key={book.id}
+           inputName={(event)=>this.changeWithInputState(event,index)}
+           />
+        );
+      });
+    }
+    
+    console.log(books);
+    return(
+      <div className="App">
+         <h1 style={style}>Book List</h1>
+         <button onClick={this.toggleBooks}>Toggle Books</button>
+         {books}
+      </div>
+    );
+  }
+}
+// Another Way Of Binding:
+// <button onClick={()=>this.changeBookState("Nineteen Eighty Four")}>Change State</button>
+export default MainComponent;
